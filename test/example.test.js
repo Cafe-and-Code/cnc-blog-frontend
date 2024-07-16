abd18a4ef06ab02a5ae03f59e889240fe972a226
Horace <49237174+MHieu128@users.noreply.github.com>
2024-07-16 12:04:20 +0700

    Clean code

--- test/example.test.js
+++
@@ -1,8 +0,0 @@
-import { describe, expect, test } from 'vitest'
-
-describe('My test', async () => {
-  test('my test', async () => {
-    const count = 1
-    expect(count).toBe(1)
-  })
-})
--- utils/Urls.js
+++
@@ -1,4 +0,0 @@
-export const Urls = {
-  BASE_URL_ELIC: 'https://api.nuxtjs.dev',
-  BASE_URL_OTHER: 'https://dev.api.example.com'
-}
